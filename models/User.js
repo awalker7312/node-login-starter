7d777c720fd3b86bb0dc29d1e2effe632a2aae54
Alan Walker <alanwalk3r@gmail.com>
2023-04-30 11:09:19 -0400

    initial commit

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+    {
+        email: { type: String, unique: true },
+        password: String,
+        role: String,
+
+        profile: {
+            firstName: String,
+            lastName: String,
+            picture: String,
+        },
+    },
+    { timestamps: true }
+);
+
+/**
+ * Password hash middleware.
+ */
+userSchema.pre("save", async function save(next) {
+    const user = this;
+    if (!user.isModified("password")) {
+        return next();
+    }
+    try {
+        user.password = await bcrypt.hash(user.password, 10);
+        next();
+    } catch (err) {
+        next(err);
+    }
+});
+
+/**
+ * Helper method for validating user's password.
+ */
+userSchema.methods.comparePassword = async function comparePassword(candidatePassword, cb) {
+    try {
+        cb(null, await bcrypt.compare(candidatePassword, this.password));
+    } catch (err) {
+        cb(err);
+    }
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
